f28722da8ae335d055c9bfeb27740b387f7e50f1
raman parge <reddyram6543@gmail.com>
2026-09-02 01:06:27 +0530

    MPU6050 driver to include accelerometer read
    Removed unused functions and added accelerometer reading functionality.

--- MPU6050/MPU6050_Driver/Core/Src/mpu6050.c
+++ MPU6050/MPU6050_Driver/Core/Src/mpu6050.c
@@ -1,33 +1,20 @@
+
 #include "mpu6050.h"
 
-
-HAL_StatusTypeDef MPU6050_IsReady(I2C_HandleTypeDef *hi2c)
-{
-
-	return HAL_OK;
-
-}
-
-
-HAL_StatusTypeDef MPU6050_ReadWhoAmI(I2C_HandleTypeDef *hi2c, uint8_t *id)
-{
-
-	return HAL_OK;
+void MPU6050_Init(I2C_HandleTypeDef *hi2c) {
+													  uint8_t data = 0x00;
+													/* Wake up MPU6050 */
+												HAL_I2C_Mem_Write(hi2c,MPU6050_ADDR,MPU6050_PWR1,1,&data,1,HAL_MAX_DELAY);
 
 }
 
-
-HAL_StatusTypeDef MPU6050_Init(I2C_HandleTypeDef *hi2c)
+void MPU6050_Read_Accel(I2C_HandleTypeDef *hi2c,int16_t *Ax,int16_t *Ay,int16_t *Az)
 {
 
-	return HAL_OK;
-
-}
-
-
-HAL_StatusTypeDef MPU6050_ReadAccel(I2C_HandleTypeDef *hi2c, MPU6050_t *mpu)
-{
-	return HAL_OK;
-
+		uint8_t buffer[6];
+		HAL_I2C_Mem_Read(hi2c,MPU6050_ADDR,ACCEL_XOUT_H,1,buffer,6,HAL_MAX_DELAY);
+		*Ax = (int16_t)(buffer[0]<<8 | buffer[1]);
+		*Ay = (int16_t)(buffer[2]<<8 | buffer[3]);
+		*Az = (int16_t)(buffer[4]<<8 | buffer[5]);
 
 }
