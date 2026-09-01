e1a0a7285dfa924866e53f11210d01e469267e33
raman parge <reddyram6543@gmail.com>
2026-09-02 01:02:01 +0530

    MPU6050 header file and update functions
    Removed redundant include guards and typedef struct for MPU6050. Updated function signatures for reading accelerometer data.

--- MPU6050/MPU6050_Driver/Core/Inc/mpu6050.h
+++ MPU6050/MPU6050_Driver/Core/Inc/mpu6050.h
@@ -4,30 +4,17 @@
  *  Created on: 22-Jun-2026
  *      Author: gauri96
  */
+#ifndef MPU6050_H
+#define MPU6050_H
 
-#ifndef __MPU6050_H__
-#define __MPU6050_H__
-
+#include "main.h"
 #include "stm32f4xx_hal.h"
+#include <stdint.h>
 
-
-
-typedef struct
-{
-    int16_t x;
-    int16_t y;
-    int16_t z;
-}MPU6050_t;
-
-
-HAL_StatusTypeDef MPU6050_IsReady(I2C_HandleTypeDef *hi2c);
-
-HAL_StatusTypeDef MPU6050_ReadWhoAmI(I2C_HandleTypeDef *hi2c,
-                                     uint8_t *id);
-
-HAL_StatusTypeDef MPU6050_Init(I2C_HandleTypeDef *hi2c);
-
-HAL_StatusTypeDef MPU6050_ReadAccel(I2C_HandleTypeDef *hi2c,
-                                    MPU6050_t *mpu);
-
+#define MPU6050_ADDR        (0x68 << 1)
+#define MPU6050_PWR1        0x6B
+#define ACCEL_XOUT_H        0x3B
+void MPU6050_Init(I2C_HandleTypeDef *hi2c);
+void MPU6050_Read_Accel(I2C_HandleTypeDef *hi2c,int16_t *Ax,int16_t *Ay,int16_t *Az);
 #endif
+
