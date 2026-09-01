2cbfd7d3aedf89dcbe6aada5f3c112622316dede
raman parge <reddyram6543@gmail.com>
2026-09-02 01:08:36 +0530

    MPU6050_Read_Accel function formatting

--- MPU6050/MPU6050_Driver/Core/Src/mpu6050.c
+++ MPU6050/MPU6050_Driver/Core/Src/mpu6050.c
@@ -6,15 +6,14 @@ void MPU6050_Init(I2C_HandleTypeDef *hi2c) {
 													/* Wake up MPU6050 */
 												HAL_I2C_Mem_Write(hi2c,MPU6050_ADDR,MPU6050_PWR1,1,&data,1,HAL_MAX_DELAY);
 
-}
+                                             }
 
 void MPU6050_Read_Accel(I2C_HandleTypeDef *hi2c,int16_t *Ax,int16_t *Ay,int16_t *Az)
-{
-
-		uint8_t buffer[6];
-		HAL_I2C_Mem_Read(hi2c,MPU6050_ADDR,ACCEL_XOUT_H,1,buffer,6,HAL_MAX_DELAY);
-		*Ax = (int16_t)(buffer[0]<<8 | buffer[1]);
-		*Ay = (int16_t)(buffer[2]<<8 | buffer[3]);
-		*Az = (int16_t)(buffer[4]<<8 | buffer[5]);
-
-}
+														
+                                                       {
+																uint8_t buffer[6];
+																HAL_I2C_Mem_Read(hi2c,MPU6050_ADDR,ACCEL_XOUT_H,1,buffer,6,HAL_MAX_DELAY);
+																*Ax = (int16_t)(buffer[0]<<8 | buffer[1]);
+																*Ay = (int16_t)(buffer[2]<<8 | buffer[3]);
+																*Az = (int16_t)(buffer[4]<<8 | buffer[5]);
+														}
